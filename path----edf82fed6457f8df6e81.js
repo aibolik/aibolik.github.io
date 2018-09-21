37f3ad00382cb4cc666f77df4e114845ac62c100
Aibol Kussain <aibolikdev@gmail.com>
2018-09-21 23:35:52 +0300

    Updates

--- path----edf82fed6457f8df6e81.js
+++ path----edf82fed6457f8df6e81.js
@@ -1,2 +1,2 @@
-webpackJsonp([60335399758886],{132:function(e,t){e.exports={data:{site:{siteMetadata:{title:"Aibol Kussain | Developer, Tech blogger"}}},layoutContext:{}}}});
+webpackJsonp([60335399758886],{136:function(e,t){e.exports={data:{site:{siteMetadata:{title:"Aibol Kussain | Developer, Tech blogger"}}},layoutContext:{}}}});
 //# sourceMappingURL=path----edf82fed6457f8df6e81.js.map
