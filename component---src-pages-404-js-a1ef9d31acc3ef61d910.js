f04f6fbc7412f1b294c23d16917c43ffb447c943
Aibol Kussain <aibolikdev@gmail.com>
2019-08-15 05:48:12 +0000

    Updates

--- component---src-pages-404-js-a1ef9d31acc3ef61d910.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{128:function(e,t,n){"use strict";n.r(t);var a=n(0),u=n.n(a);t.default=function(){return u.a.createElement("div",null,u.a.createElement("h1",null,"NOT FOUND"),u.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
-//# sourceMappingURL=component---src-pages-404-js-a1ef9d31acc3ef61d910.js.map
