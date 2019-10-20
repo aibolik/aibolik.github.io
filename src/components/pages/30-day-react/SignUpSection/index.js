57f02b2b0daba6696dd2e6936aae763366fda1b4
Aibol Kussain <aibolikdev@gmail.com>
2019-10-20 08:38:54 +0200

    feat(30-day-react): setup signup button's functionality

--- src/components/pages/30-day-react/SignUpSection/index.js
+++ src/components/pages/30-day-react/SignUpSection/index.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
 import Box from '../../../Box';
 import Text from '../../../Text';
 
 import Button from '../../../PromoButton';
+
+const COURSE_SIGNUP_URL = 'https://europe-west1-my-blog-aibolik.cloudfunctions.net/courseSignUp';
 
 const Input = styled.input`
   border: 1px solid rgba(0, 0, 0, 0.24);
@@ -14,6 +16,37 @@ const Input = styled.input`
 `;
 
 const SignUp = () => {
+  const [input, setInput] = useState('');
+  const [isLoading, setLoading] = useState(false);
+  const [shouldSignUp, setSignUp] = useState(false);
+
+  useEffect(() => {
+    if (shouldSignUp) {
+      setLoading(true);
+      fetch(COURSE_SIGNUP_URL, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ email: input })
+      })
+      .then(res => res.json())
+      .then(res => {
+        if (!res.data) {
+          throw new Error('Something went wrong');
+        }
+        alert('Thanks for signing up. You will receive email when course is released');
+        setInput('');
+      })
+      .catch(err => {
+        alert(err.message);
+      })
+      .finally(() => {
+        setLoading(false);
+        setSignUp(false);
+      })
+    }
+  }, [shouldSignUp]);
 
   return (
     <Box px={3} mb={4} as="section" id="sign-up">
@@ -23,10 +56,13 @@ const SignUp = () => {
         <Text as="span" fontWeight="700">free access </Text>
         when course releases
       </Text>
-      <form>
+      <form onSubmit={e => {
+        e.preventDefault();
+        setSignUp(true);
+      }}>
         <Box display="flex" flexDirection="column">
-          <Input placeholder="email@example.com" />
-          <Button minWidth="220px" border="none" bg="#1c7ee0" color="#fff" fontWeight="600" mt={3} type="submit">Sign Up</Button>
+          <Input placeholder="email@example.com" value={input} onChange={e => setInput(e.target.value)} />
+          <Button minWidth="220px" border="none" bg={isLoading ? '#5394d6' : '#1c7ee0'} disabled={isLoading} color="#fff" fontWeight="600" mt={3} type="submit">Sign Up</Button>
         </Box>
       </form>
     </Box>
